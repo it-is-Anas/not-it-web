5d56418ee4a6c5b98087eff0d168307d98a7cd5a
prontoTheLegend <anasprog8@gmail.com>
2025-06-05 00:45:50 +0300

    log in process done (still want to save data to state mangement)

--- src/pages/Log/LogIn.tsx
+++ src/pages/Log/LogIn.tsx
@@ -4,6 +4,7 @@ import { Link  } from "react-router-dom";
 
 import { emailValidation , passwordValidation } from '../../validations/validations';
 import { useState } from "react";
+import axios from "axios";
 
 export default function LogIn(){
 
@@ -29,8 +30,35 @@ function passwordValidate(value:string){
 }
 
     
-    function getEmail(value: string):void{
-        console.log(value);
+    const [user , setUser] = useState({email: '',password: ''});
+
+    function setEmail(email){
+        setUser(oldUser=>({...oldUser,email}));
+    }
+
+    function setPassword(password){
+        setUser(oldUser=>({...oldUser,password}));
+    }
+
+    const logIn = async () => {
+        try{
+            console.log(user);
+            const response = await axios.post('http://localhost:3000/auth/log-in', user, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if(response.status ===  201){
+                const {access_token , user: data, } = response.data;
+                console.log(access_token,user); // I have to save this to state mangement
+            }
+        }catch(err){
+            console.log('Something went wrong please try again later');
+            console.log(err);
+            if(err.response.status){
+                console.log(err.response.data.message[1]);
+            }
+        }
     }
 
     return (
@@ -41,9 +69,9 @@ function passwordValidate(value:string){
             <div className="w-[100%] h-[100%] flex items-center justify-center  ">
                 <div className="flex items-center flex-col justify-around p-[0.5em] w-[25em] min-h-[27em] rounded-[10px] bg-[#eee] max-[767px]:w-[100%] max-[767px]:h-[100%]  max-[767px]:rounded-[0]  ">
                     <p className="p-2  text-[1.5em] font-bold">Log In</p>
-                    <InputFiled placeholder="Jhon@example.com" type='email' label="E-mail" validationFn={emailValidate} getValueFn={getEmail}  />
-                    <InputFiled placeholder="********"   type='password' label="password" validationFn={passwordValidate}  />
-                    <Btn label="Log In" />
+                    <InputFiled  placeholder="Jhon@example.com" type='email' label="E-mail" validationFn={emailValidate} getValueFn={setEmail}  />
+                    <InputFiled placeholder="********"   type='password' label="password" validationFn={passwordValidate} getValueFn={setPassword}  />
+                    <Btn onClick={logIn} label="Log In" />
                     <Link className="text-[#7986cb] text-[15px] font-[500]" to='/log/sign-up' >Sign up</Link>
                     <p className="text-[#ff7b64] text-center w-[80%] text-[.9em] font-[100px] " >{ validateError.msg }</p>
                 </div>
